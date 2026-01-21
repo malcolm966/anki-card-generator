9c45fd684e31ae6e2867d91c7fc299b482124e21
malcolm <malcolm19880513@gmail.com>
2026-01-21 15:25:52 +0900

    dev: 将jp2000数据导入数据库

---
+++ src/csv_util.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pathlib import Path
+from db_util import create_vocabulary_table, insert_vocabulary
+RESOURCES_DIR = Path(__file__).parent.parent / "resources"
+CSV_FILE = RESOURCES_DIR / 'japanese_vocabulary_2000.csv'
+df = pd.read_csv(
+    CSV_FILE,
+    engine="python",
+    sep="\t",
+    encoding="utf-8"
+)
+create_vocabulary_table()
+for _, row in df.iterrows():
+    word = row["Word"]
+    meaning = row["Meaning"]
+    transliteration = row["Transliteration"]
+    sentences = row["Example Sentence"]
+    insert_vocabulary(word=word,meaning=meaning,sentences=sentences,transliteration=transliteration)
--- src/db_util.py
+++ src/db_util.py
@@ -1,5 +1,29 @@
 import sqlite3
 
+def create_vocabulary_table():
+    conn = sqlite3.connect("jp.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS vocabulary (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        word TEXT,        
+        transliteration TEXT,
+        meaning TEXT,
+        sentences TEXT
+    )
+    """) 
+
+def insert_vocabulary(word, transliteration, meaning, sentences):
+    conn = sqlite3.connect("jp.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO vocabulary (word,transliteration, meaning, sentences) VALUES (?,?, ?, ?)",
+        (word,transliteration, meaning, sentences)
+    )
+
+    conn.commit()
+    conn.close()
+
 
 def create_grammar_table():
     conn = sqlite3.connect("jp.db")
